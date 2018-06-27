2341e82a0f067f1622f50b531a2c9e5bbd25f440
Arjan Molenaar <gaphor@gmail.com>
2018-06-27 22:16:44 +0200

    encoding seems to work

--- lib/enigma_machine.ex
+++ lib/enigma_machine.ex
@@ -3,16 +3,36 @@ defmodule EnigmaMachine do
   Documentation for EnigmaMachine.
   """
 
-  @doc """
-  Hello world.
+  import Enum
 
-  ## Examples
+  @alphabet 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-      iex> EnigmaMachine.hello
-      :world
+  def rotor(1), do: {'EKMFLGDQVZNTOWYHXUSPAIBRCJ', ?Q}
+  def rotor(2), do: {'AJDKSIRUXBLHWTMCQGZNPYFVOE', ?E}
+  def rotor(3), do: {'BDFHJLCPRTXVZNYEIWGAKMUSQO', ?V}
 
-  """
-  def hello do
-    :world
+  def ukw(:b), do: 'YRUHQSLDPXNGOKMIEBFZCWVJAT'
+
+
+  def encode_char(input, {rotor, offset}) do
+    Enum.at(rotor, input - ?A + offset |> Integer.mod(Enum.count(rotor)))
   end
+
+  def reverse_rotor(rotor) do
+    Enum.zip(rotor, @alphabet)
+    |> Enum.sort
+    |> Enum.map(fn {_a, b} -> b end) |> IO.inspect
+  end
+
+  def reverse_encode(input, {rotor, _notch}, offset) do
+    Enum.at(rotor, input - ?A + offset |> Integer.mod(Enum.count(rotor)))
+  end
+
+  def encode([input | _text], offsets, [ukw | rotors]) do
+    rotor_offsets = zip(rotors, offsets)
+    all_rotors = map(rotor_offsets |> reverse, fn {{r, _n}, o} -> {reverse_rotor(r), o} end) ++ [{ukw, 0}] ++ map(rotor_offsets, fn {{r, _n}, o} -> {r, o} end)
+
+    [all_rotors |> reduce(input, &(encode_char(&2, &1)))]
+  end
+
 end
--- test/enigma_machine_test.exs
+++ test/enigma_machine_test.exs
@@ -2,7 +2,35 @@ defmodule EnigmaMachineTest do
   use ExUnit.Case
   doctest EnigmaMachine
 
-  test "greets the world" do
-    assert EnigmaMachine.hello() == :world
+  import EnigmaMachine
+
+  test "encode a character" do
+    assert EnigmaMachine.encode_char(?A, {'BDAC', 0}) == ?B
+    assert EnigmaMachine.encode_char(?A, {'BDAC', 1}) == ?D
+    assert EnigmaMachine.encode_char(?D, {'BDAC', 1}) == ?B
+    assert EnigmaMachine.encode_char(?C, {'BDAC', 3}) == ?D
   end
+
+  test "reverse rotor" do
+    assert EnigmaMachine.reverse_rotor('BDAC') == 'CADB'
+  end
+
+  test "rotor rotation" do
+
+  end
+
+  test "encode wone char with default settings" do
+    # assert EnigmaMachine.encode('A', {0}, [@rotor_1], @ukw_b) == 'H'
+    # assert EnigmaMachine.encode('H', {0}, [@rotor_1], @ukw_b) == 'A'
+    # assert EnigmaMachine.encode('A', {0,0}, [@rotor_2, @rotor_1], @ukw_b) == 'N'
+    # assert EnigmaMachine.encode('H', {0,0}, [@rotor_2, @rotor_1], @ukw_b) == 'J'
+    # assert encode('HELLOWORLD', [0,0,0], [ukw(:b), rotor(3), rotor(2), rotor(1)]) == 'MFNCZBBFZM'
+    # assert encode('HELLOWORLD', [0,0,0], [ukw(:b), rotor(1), rotor(3), rotor(2)]) == 'ZXVMIZYFEY'
+  end
+
+  test "encode with default settings" do
+    # assert EnigmaMachine.encode('HELLOWORLD') == 'MFNCZBBFZM'
+  end
+
+
 end
